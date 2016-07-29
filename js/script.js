a84c64f45e536baa66c1a55c53491d725fa5705d
Anton <toshidono.it.study@gmail.com>
2016-07-29 16:07:47 +0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/**
+ * Created by anton on 04.07.16.
+ */
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top-40}, 1200);
+    });
+});
